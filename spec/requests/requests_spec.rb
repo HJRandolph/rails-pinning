b1c17932c2fa18f6776a98dcd3fa1092a3e273d2
HJRandolph <hjrandolph@gmail.com>
2017-07-06 21:47:43 -0400

    Adding named route for show_by_name route

--- spec/requests/requests_spec.rb
+++ spec/requests/requests_spec.rb
@@ -1,5 +1,23 @@
 require "spec_helper"
 
 RSpec.describe "Our Application Routes" do
-  
+
+	describe "get /pins/name-:slug" do
+		
+		it "renders the pins/show template" do
+			pin = Pin.first
+			get "/pins/name-#{pin.slug}"
+			expect(response).to render_template("pins/show")
+		end
+		
+		it "populates the @pin variable with the appropriate pin" do
+			pin = Pin.first
+			get "/pins/name-#{pin.slug}"
+			expect(assigns[:pin]).to eq(Pin.find(1))
+		end
+		
+	end
+
+
+################################### The Last End ###################################
 end
