ffe9de32efe3fed594768a36443d751319aaf41f
HJRandolph <hjrandolph@gmail.com>
2017-07-06 20:04:41 -0400

    Adding previously untracked files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703040829) do
+ActiveRecord::Schema.define(version: 20170760022457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170703040829) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "resource_type"
   end
 
   add_index "pins", ["category_id"], name: "index_pins_on_category_id", using: :btree
