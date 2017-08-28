3673ea93c7e3f122c9df9c7a38430d98c9ed8495
HJRandolph <hjrandolph@gmail.com>
2017-08-28 12:40:04 -0400

    replacing the connection

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,12 +1,12 @@
 class Pin < ActiveRecord::Base
-  #belongs_to :category
+  belongs_to :category
   
   validates_presence_of :title, :url, :slug, :text, :category_id
   validates_uniqueness_of :slug
 
-  #has_attached_file :image, styles: { medium: "300x300", thumb: "75x75" },
-  #default_url: "http://placebear.com/200/300"
-  #validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  has_attached_file :image, styles: { medium: "300x300", thumb: "75x75" },
+  default_url: "http://placebear.com/200/300"
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
 
 ################################### The Last End ###################################  
