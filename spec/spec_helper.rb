db9c7aa9798df97ecc0ab32ba1d490b0201250a1
HJRandolph <hjrandolph@gmail.com>
2017-09-17 12:00:24 -0400

    Updating tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,13 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-#require 'database_cleaner'
+
 
 
 
 RSpec.configure do |config|
+
+
 
 config.before(:all) do
   FactoryGirl.reload
