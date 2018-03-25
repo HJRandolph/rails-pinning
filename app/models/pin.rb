74a20651bf1a7d2c640ba96a7b7aba1a66facdf4
HJRandolph <hjrandolph@gmail.com>
2018-03-25 17:53:53 -0400

    updating filename requirement for paperclip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -14,6 +14,7 @@ class Pin < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300", thumb: "60x60" }, 
   default_url: "http://placebear.com/200/300"
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  validates_attachment_file_name :image, matches: [/png\z/, /jpe?g\z/]
   validate :image_size
   
   
